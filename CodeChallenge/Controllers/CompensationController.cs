7844efc00ce259fc017f76f88f937a9caf8dc958
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2025-02-01 15:00:38 -0500

    Adding the skeleton for the /api/compensation endpoints

---
+++ CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,43 @@
+﻿using CodeChallenge.Models;
+using CodeChallenge.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CodeChallenge.Controllers
+{
+    [Route("api/compensation")]
+    [ApiController]
+    public class CompensationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+
+        public CompensationController(ILogger<CompensationController> logger, IEmployeeService employeeService)
+        {
+            // TODO: Validate these
+            //if (logger == null) { throw new ArgumentNullException($"The parameter '{nameof(logger)}' was null or otherwise invalid"); }
+            //if (employeeService == null) { throw new ArgumentNullException($"The parameter '{nameof(employeeService)}' was null or otherwise invalid"); }
+
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+
+
+        [HttpGet("{id}", Name = "getCompensationByEmployeeById")]
+        public IActionResult GetCompensationByEmployeeId(String id)
+        {
+            Compensation compensation;
+
+            _logger.LogDebug($"Received compensation get request for employee '{id}'");
+
+            // TODO: Flesh out the real logic here
+
+
+            return NotFound();
+        }
+    }
+}
---
+++ CodeChallenge/Models/Compensation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CodeChallenge.Models
+{
+    public class Compensation
+    {
+        public Employee Employee { get; set; }
+        public float Salary { get; set; }
+        public DateOnly EffectiveDate { get; set; }
+    }
+}
