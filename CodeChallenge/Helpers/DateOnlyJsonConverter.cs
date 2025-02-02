cbadd190ed27aae7152f8383798648ba9d9aa45a
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2025-02-02 18:58:43 -0500

    Added direction for how to test DateOnlyJsonConverter

--- CodeChallenge/Helpers/DateOnlyJsonConverter.cs
+++ CodeChallenge/Helpers/DateOnlyJsonConverter.cs
@@ -4,18 +4,24 @@ using System.Text.Json.Serialization;
 
 namespace CodeChallenge.Helpers
 {
-    // .NET 6 does not provide serialization for a DateOnly datatype, so this class takes care of it.
+
+
+
+    // .NET 6 does not provide serialization for a DateOnly datatype OOTB, so this class takes care of that.
+    // Please also see App.AddServices() where this class is registered.
     // Source: https://stackoverflow.com/questions/74246482/system-notsupportedexception-serialization-and-deserialization-of-system-dateo
+
+    // TODO: Write unit tests for this converter.  I found an article where the author created extension methods to encapsulate
+    //       the stream operations needed: https://khalidabuhakmeh.com/systemtextjson-jsonconverter-test-helpers
+    //       Adapt those for this converter, and then write tests to verify the logic.
+
     public class DateOnlyJsonConverter: JsonConverter<DateOnly>
     {
-
-        // TODO: Unit tests for this
         public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             return DateOnly.FromDateTime(reader.GetDateTime());
         }
 
-        // TODO: Unit tests for this
         public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
         {
             var isoDate = value.ToString("O");
